b4952219bdc61a246e46d2b3ade93263f7b828b1
Muhammad Sohail <146514678+MuhammadSohailA@users.noreply.github.com>
2023-10-01 04:41:24 +0500

    /SADL,A
    AS/;DAS;LD

---
+++ day_2/script.py
@@ -0,0 +1,7 @@
+print("Hello Word!")
+
+# print("This is python course for new joinee")
+
+print("this is Data Science course")
+
+# upper all three line of code do same thing show string on the screen
---
+++ day_3/oprators.py
@@ -0,0 +1,7 @@
+print("this is oprators class")
+
+
+# PEMDAS   (PERNTHASIS, EXPONENT, MULTIPLICATION, DIVIDE, ADDITION, SUBTRACTION)
+print(3**2/2*3/(2*6))
+
+
