2dd1b2b129160a2f9c3450961cafe7d55a0ccb73
Muhammad Sohail <146514678+MuhammadSohailA@users.noreply.github.com>
2023-10-02 22:28:16 +0000

    new class 05_varables here

--- day_3/oprators.py
+++
@@ -1,7 +0,0 @@
-print("this is oprators class")
-
-
-# PEMDAS   (PERNTHASIS, EXPONENT, MULTIPLICATION, DIVIDE, ADDITION, SUBTRACTION)
-print(3**2/2*3/(2*6))
-
-
--- day_4/script.py
+++
@@ -1,7 +0,0 @@
-print("This is Muhammad Sohail")
-
-print(2+3)
-print(4*8)
-print(5//9)
-
-print(8**9)
